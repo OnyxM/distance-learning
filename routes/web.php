da1db5b2f737cff06b6482b4794b509fe780fdce
onyxM <onesimemoffo@gmail.com>
2022-01-31 20:22:15 +0100

    fixing an error

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, "index"])->name("index");
-Route::get('/courses', [HomeController::class, "courses"])->name("courses");
+Route::get('/courses', function(){
+    return redirect()->route('index');
+})->name("courses");
+// Route::get('/courses', [HomeController::class, "courses"])->name("courses");
 Route::get('/about', [HomeController::class, "about"])->name("about");
 
 Auth::routes();
